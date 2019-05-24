8cf792f5adcf8511eb2c69217374e5ee525c0bb7
Chunpeng Huo <huocp@me.com>
2019-05-24 12:38:34 +1000

    chore: upgrade packages, remove through2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const through = require('through2');
+const {Transform} = require('stream');
 const PluginError = require('plugin-error');
 const path = require('path');
 const {createDefaultCompiler, assemble} = require('@vue/component-compiler');
@@ -14,49 +14,52 @@ module.exports = function (opts) {
 
   const vueCompiler = createDefaultCompiler({script, style, template, resolve});
 
-  return through.obj(function(file, enc, cb) {
-    if (file.isStream()) {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming is not supported'));
-      return;
-    }
+  return new Transform({
+    objectMode: true,
+    transform: function(file, enc, cb) {
+      if (file.isStream()) {
+        this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming is not supported'));
+        return;
+      }
 
-    if (file.isBuffer()) {
-      if (file.extname === '.vue') {
-        const content = file.contents.toString();
+      if (file.isBuffer()) {
+        if (file.extname === '.vue') {
+          const content = file.contents.toString();
 
-        // need to use localPath instead of file.relative (relative to file.base)
-        // as vue compiler needs the path to be related to process.cwd() in order
-        // for additional resources locating.
-        const localPath = path.relative(file.cwd, file.path);
+          // need to use localPath instead of file.relative (relative to file.base)
+          // as vue compiler needs the path to be related to process.cwd() in order
+          // for additional resources locating.
+          const localPath = path.relative(file.cwd, file.path);
 
-        const result = assemble(
-          vueCompiler,
-          localPath,
-          vueCompiler.compileToDescriptor(localPath, content),
-          {normalizer, styleInjector, styleInjectorSSR}
-        );
-
-        file.extname = '.js';
-        file.contents = Buffer.from(result.code);
-
-        const {map} = result;
-        if (file.sourceMap) {
-          // With .vue file only contains <template> node,
-          // @vue/component-compiler doesn't insert sourcesContent.
-          // I don't quite understand.
-          if (!map.sourcesContent) map.sourcesContent = [content];
-          // Normalize file path to relative to base.
-          // This is to match the default behaviour of gulp source map.
-          map.sources = map.sources.map(f =>
-            normalize(path.relative(file.base, path.resolve(file.cwd, f)))
+          const result = assemble(
+            vueCompiler,
+            localPath,
+            vueCompiler.compileToDescriptor(localPath, content),
+            {normalizer, styleInjector, styleInjectorSSR}
           );
-          map.file = normalize(file.relative);
-          applySourceMap(file, map);
+
+          file.extname = '.js';
+          file.contents = Buffer.from(result.code);
+
+          const {map} = result;
+          if (file.sourceMap) {
+            // With .vue file only contains <template> node,
+            // @vue/component-compiler doesn't insert sourcesContent.
+            // I don't quite understand.
+            if (!map.sourcesContent) map.sourcesContent = [content];
+            // Normalize file path to relative to base.
+            // This is to match the default behaviour of gulp source map.
+            map.sources = map.sources.map(f =>
+              normalize(path.relative(file.base, path.resolve(file.cwd, f)))
+            );
+            map.file = normalize(file.relative);
+            applySourceMap(file, map);
+          }
         }
       }
-    }
 
-    cb(null, file);
+      cb(null, file);
+    }
   });
 };
 
